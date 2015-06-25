f9092e5c99b418dace10b86a4c77a2603a60ec92
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-06-24 23:38:40 -0400

    artist model spec

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,15 +1,17 @@
-# == Schema Information
-#
-# Table name: artists
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'rails_helper'
 
 describe Artist do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject(:artist) { FactoryGirl.create(:artist, name: "Foo") }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:albums) }
+  it { should respond_to(:songs) }
+
+  it { should be_valid }
+
+  context "when name is not present" do
+    before { artist.name = nil }
+    it { should_not be_valid }
+  end
 end
