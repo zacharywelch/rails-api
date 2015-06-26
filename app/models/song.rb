9e3a7c83f1edf49e3a0bb266ed29b68552c09e5e
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-06-26 00:59:00 -0400

    refactor branch

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,8 +11,15 @@
 #
 
 class Song < ActiveRecord::Base
-  has_ranking
   belongs_to :album
 
   validates :name, presence: true
+
+  scope :ranked, -> { order(:rank) }
+  scope :top, ->(n) { ranked.limit(n) }
+  scope :hot, -> { top(5) }
+
+  def hot?
+    rank.between?(1, 5)
+  end
 end
--- spec/support/shared_examples_for_ranked.rb
+++ spec/support/shared_examples_for_ranked.rb
@@ -28,4 +28,28 @@ RSpec.shared_examples "ranked" do
       expect(described_class.top(2).second.rank).to be 2
     end
   end
+
+  describe "#hot" do
+    
+    context "when ranked in top 5" do
+      before { subject.rank = 5 }
+      it { should be_hot }
+    end
+
+    context "when ranked below 5" do
+      before { subject.rank = 6 }
+      it { should_not be_hot }
+    end
+  end
+
+  describe ".hot" do
+
+    before do 
+      10.times { |i| FactoryGirl.create(factory, rank: i + 1) }
+    end
+    
+    it "returns the top 5 #{described_class.model_name.plural}" do
+      expect(described_class.hot.map(&:rank)).to eq [1,2,3,4,5]
+    end
+  end  
 end
