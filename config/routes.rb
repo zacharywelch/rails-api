6d8d42efc16ee596343a42485b889b6ed700b3a8
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-07-05 12:02:10 -0400

    cleanup collection routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
   scope defaults: { format: :json } do
     resources :artists, except: [:new, :edit] do
-      get :featured, on: :collection
-      get :ranked, on: :collection
-      get :hot, on: :collection
+      get :featured, :ranked, :hot, on: :collection
       resources :albums, only: [:index, :create]
     end
     resources :albums, except: [:new, :edit, :create] do
-      get :recent, on: :collection
-      get :ranked, on: :collection
-      get :hot, on: :collection
+      get :recent, :ranked, :hot, on: :collection
       resources :songs, only: [:index, :create]
     end
     resources :songs, except: [:new, :edit, :create] do
-      get :top_25, on: :collection
-      get :hot, on: :collection 
+      get :top_25, :hot, on: :collection
     end
     root to: 'artists#index'
   end
