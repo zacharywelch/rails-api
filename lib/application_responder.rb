2aa43031a766099e1ea5128ce9ed1c7c4a1fe590
Zachary Welch <zwelch154@gmail.com>
2017-05-26 21:14:02 -0400

    update gems

--- lib/application_responder.rb
+++ lib/application_responder.rb
@@ -1,5 +1,5 @@
 class ApplicationResponder < ActionController::Responder
   include Responders::HttpCacheResponder
-  include Responders::JsonResponder
-  include Responders::PaginationResponder
+  # include Responders::JsonResponder
+  include Responders::PaginateResponder
 end
